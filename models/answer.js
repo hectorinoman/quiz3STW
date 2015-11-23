7e7f49a1fe9ce4bf4b0c3e1979fe8b643ab3ae12
hectorinoman <hectorinoman@gmail.com>
2015-11-23 16:39:16 +0000

    primera pregunta funcional, a falta de la vista

--- models/answer.js
+++ models/answer.js
@@ -4,7 +4,7 @@ function Answer(ans){
     return function(x){return x === this.ans;};  
   }
   else if (this.ans instanceof RegExp) {
-    return function(x){return (this.ans).exec(x);};
+    return function(x){return x.match(ans);};
   }
   else {
     return ans;
