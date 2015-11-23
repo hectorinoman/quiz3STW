bd82fb54190d4e6fcce0aa4bd8404fc98107c0cb
hectorinoman <hectorinoman@gmail.com>
2015-11-23 16:12:54 +0000

    clase question

---
+++ models/question.js
@@ -0,0 +1,40 @@
+function Question(ques){
+  this.ques = ques;
+  this.kind = kind;
+}
+
+function ShortQuestion(ques){
+  Question.call(this, ques);
+  kind = "short";
+}
+
+ShortQuestion.prototype.getKind = function(){
+  return this.kind;
+}
+
+ShortQuestion.prototype.getQues = function(){
+  return this.ques;
+}
+
+function LargeQuestion(ques){
+  Question.call(this, ques);
+  kind = "large";
+}
+
+LargeQuestion.prototype.getKind = function(){
+  return this.kind;
+}
+
+LargeQuestion.prototype.getQues = function(){
+  return this.ques;
+}
+
+ShortQuestion.prototype = new Question();
+ShortQuestion.prototype.constructor = ShortQuestion;
+LargeQuestion.prototype = new Question();
+LargeQuestion.prototype.constructor = LargeQuestion;
+
+
+module.exports = Question;
+module.exports = ShortQuestion;
+module.exports = LargeQuestion;
